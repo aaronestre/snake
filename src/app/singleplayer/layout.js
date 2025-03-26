eb78acb765e219fd3999393b619ab1576824d45b
aaronestre <aaron.estrellado@gmail.com>
2025-03-25 20:53:59 -0400

    Add home screen

---
+++ src/app/singleplayer/layout.js
@@ -0,0 +1,17 @@
+
+export const metadata = {
+  title: "Snake Battle",
+  description: "A multiplayer snake game",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/singleplayer/page.js
@@ -0,0 +1,9 @@
+import Board from "../components/board";
+
+export default function Home() {
+  return (
+    <div className="bg-(--primary-dark) w-full h-screen flex items-center justify-center">
+      <Board />
+    </div>
+  );
+}
