d0cd9e1240924bfd6d64fe3543e031453143653a
aaronestre <aaron.estrellado@gmail.com>
2025-03-31 19:08:20 -0400

    Remove unused import

--- frontend/src/app/hooks/useGameManager.js
+++ frontend/src/app/hooks/useGameManager.js
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
 
 import { useSnakeStore } from "../stores/snakeStore";
-import { useControlsStore } from "../stores/controlsStore";
 import { useGameEngineStore } from "../stores/gameEngineStore";
 
 export const useGameManager = (speed = 75) => {
 	const moveSnake = useSnakeStore((state) => state.moveSnake);
-	const moveVector = useControlsStore((state) => state.moveVector);
 	const gameStatus = useGameEngineStore((state) => state.gameStatus);
 
 	const initialize = useSnakeStore((state) => state.initializeSnake);
